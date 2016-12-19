5403bbdb41cd7155d8ddbe9aaa14434897117506
tomkelsey <tom@kelsey.me>
2016-12-19 15:03:20 +0000

    Fixed typo ref emojione

--- src/Emoji/index.js
+++ src/Emoji/index.js
@@ -5,7 +5,7 @@ import { toShort, emojioneList } from 'emojione';
 const Emoji = ({ theme = {}, cacheBustParam, imagePath, imageType, className, decoratedText, ...props }) => {
   const shortName = toShort(decoratedText);
   // short name to image url code steal from emojione source code
-  const shortNameForImage = emojioneList[shortName].unicode[emojione.emojioneList[shortName].unicode.length - 1];
+  const shortNameForImage = emojioneList[shortName].unicode[emojioneList[shortName].unicode.length - 1];
   const backgroundImage = `url(${imagePath}${shortNameForImage}.${imageType}${cacheBustParam})`;
   const combinedClassName = unionClassNames(theme.emoji, className);
   const characterClassName = unionClassNames(theme.emojiCharacter);
